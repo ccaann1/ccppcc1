f011f57536e7849e76dad0d3415efa955b3defae
ccaann1 <bhk955@gmail.com>
2025-01-09 21:24:34 -0500

    Update cp1.py
    Added CancePro Menu

--- cp1.py
+++ cp1.py
@@ -5,6 +5,45 @@ import requests
 
 st.markdown(
     """
+        
+		<!-- Favicon -->
+        <link rel="icon" href="https://www.cancepro.com/img/favicon.png">
+
+		<!-- Google Fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://www.cancepro.com/css/bootstrap.min.css">
+		<!-- Nice Select CSS -->
+		<link rel="stylesheet" href="https://www.cancepro.com/css/nice-select.css">
+		<!-- Font Awesome CSS -->
+        <link rel="stylesheet" href="https://www.cancepro.com/css/font-awesome.min.css">
+		<!-- icofont CSS -->
+        <link rel="stylesheet" href="https://www.cancepro.com/css/icofont.css">
+		<!-- Slicknav -->
+		<link rel="stylesheet" href="https://www.cancepro.com/css/slicknav.min.css">
+		<!-- Owl Carousel CSS -->
+        <link rel="stylesheet" href="https://www.cancepro.com/css/owl-carousel.css">
+		<!-- Datepicker CSS -->
+		<link rel="stylesheet" href="https://www.cancepro.com/css/datepicker.css">
+		<!-- Animate CSS -->
+        <link rel="stylesheet" href="https://www.cancepro.com/css/animate.min.css">
+		<!-- Magnific Popup CSS -->
+        <link rel="stylesheet" href="https://www.cancepro.com/css/magnific-popup.css">
+
+		<!-- Medipro CSS -->
+        <link rel="stylesheet" href="https://www.cancepro.com/css/normalize.css">
+        <link rel="stylesheet" href="https://www.cancepro.com/style.css">
+        <link rel="stylesheet" href="https://www.cancepro.com/css/responsive.css">
+
+
+    """,
+    unsafe_allow_html=True
+)
+
+
+st.markdown(
+    """
     <style>
     header {visibility: hidden;}
     #MainMenu {visibility: hidden;}
@@ -32,6 +71,51 @@ st.markdown(
 response = requests.get('https://canceproit.pythonanywhere.com/ttthais')
 data = response.json()
 take_this = data[0]
+
+
+st.markdown(
+    """
+        <!-- Header Area -->
+		<header class="header" id="header">
+			<!-- Header Inner -->
+			<div class="header-inner">
+				<div class="container">
+					<div class="inner">
+						<div class="row">
+							<div class="col-lg-5 col-md-3 col-12">
+								<!-- Start Logo -->
+								<div class="logo">
+									<a href="index.html"><img src="https://www.cancepro.com/img/icon/CancePro_Icon.png" alt="#"></a>
+								</div>
+								<!-- End Logo -->
+								<!-- Mobile Nav -->
+								<div class="mobile-nav"></div>
+								<!-- End Mobile Nav -->
+							</div>
+							<div class="col-lg-7 col-md-9 col-12">
+								<!-- Main Menu -->
+								<div class="main-menu">
+									<nav class="navigation">
+										<ul class="nav menu" style="float: right;">
+											<li><a href="https://www.cancepro.com">Go Home</a></li>
+											<li><a href="https://canceproit.pythonanywhere.com/">Ask Me </a></li>
+											<li><a href="https://canceproit.pythonanywhere.com/getxray">X-Ray Analysis</a></li>
+											<li><a href="https://canceproit.pythonanywhere.com/getliveanalysis">Cancer Records</a></li>
+										</ul>
+									</nav>
+								</div>
+								<!--/ End Main Menu -->
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--/ End Header Inner -->
+		</header>
+		<!-- End Header Area -->
+    """,
+    unsafe_allow_html=True
+)
 
 # Show title and description.
 st.title("CancePro")
@@ -91,3 +175,75 @@ if prompt := st.chat_input("What is up?"):
     with st.chat_message("assistant"):
         response = st.write_stream(stream)
     st.session_state.messages.append({"role": "assistant", "content": response})
+
+
+st.markdown(
+    """
+        <!-- Footer Area -->
+		<footer id="footer" class="footer ">
+			<!-- Footer Top -->
+			<!--/ End Footer Top -->
+			<!-- Copyright -->
+			<div class="copyright">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-12 col-md-12 col-12">
+							<div class="copyright-content">
+								<p> © Copyright 2024  |  All Rights Reserved by <a href="#header">cancepro.com</a> </p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--/ End Copyright -->
+		</footer>
+		<!--/ End Footer Area -->
+    """,
+    unsafe_allow_html=True
+)
+
+st.markdown(
+    """
+        <!-- jquery Min JS -->
+        <script src="https://www.cancepro.com/js/jquery.min.js"></script>
+		<!-- jquery Migrate JS -->
+		<script src="https://www.cancepro.com/js/jquery-migrate-3.0.0.js"></script>
+		<!-- jquery Ui JS -->
+		<script src="https://www.cancepro.com/js/jquery-ui.min.js"></script>
+		<!-- Easing JS -->
+        <script src="https://www.cancepro.com/js/easing.js"></script>
+		<!-- Color JS -->
+		<script src="https://www.cancepro.com/js/colors.js"></script>
+		<!-- Popper JS -->
+		<script src="https://www.cancepro.com/js/popper.min.js"></script>
+		<!-- Bootstrap Datepicker JS -->
+		<script src="https://www.cancepro.com/js/bootstrap-datepicker.js"></script>
+		<!-- Jquery Nav JS -->
+        <script src="https://www.cancepro.com/js/jquery.nav.js"></script>
+		<!-- Slicknav JS -->
+		<script src="https://www.cancepro.com/js/slicknav.min.js"></script>
+		<!-- ScrollUp JS -->
+        <script src="https://www.cancepro.com/js/jquery.scrollUp.min.js"></script>
+		<!-- Niceselect JS -->
+		<script src="https://www.cancepro.com/js/niceselect.js"></script>
+		<!-- Tilt Jquery JS -->
+		<script src="https://www.cancepro.com/js/tilt.jquery.min.js"></script>
+		<!-- Owl Carousel JS -->
+        <script src="https://www.cancepro.com/js/owl-carousel.js"></script>
+		<!-- counterup JS -->
+		<script src="https://www.cancepro.com/js/jquery.counterup.min.js"></script>
+		<!-- Steller JS -->
+		<script src="https://www.cancepro.com/js/steller.js"></script>
+		<!-- Wow JS -->
+		<script src="https://www.cancepro.com/js/wow.min.js"></script>
+		<!-- Magnific Popup JS -->
+		<script src="https://www.cancepro.com/js/jquery.magnific-popup.min.js"></script>
+		<!-- Counter Up CDN JS -->
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>
+		<!-- Bootstrap JS -->
+		<script src="https://www.cancepro.com/js/bootstrap.min.js"></script>
+		<!-- Main JS -->
+		<script src="https://www.cancepro.com/js/main.js"></script>
+    """,
+    unsafe_allow_html=True
+)
