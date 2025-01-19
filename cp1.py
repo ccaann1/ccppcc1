509e8c35bd09f4342830b62b7abce4374fd3fa35
ccaann1 <bhk955@gmail.com>
2025-01-19 02:13:04 -0500

    Update cp1.py
    mt-70

--- cp1.py
+++ cp1.py
@@ -182,7 +182,7 @@ st.markdown(
                 background-color: #ffffff !important;
                 # color:#ffffff;
                 position: absolute;
-                margin-bottom: -10%;
+                margin-top: 70%;
                 /* margin-left: -100px;
             }
         }
