76383c550fa77a00550e1a9771770239f4c9e9ed
ccaann1 <bhk955@gmail.com>
2025-01-19 04:27:33 -0500

    Update cp1.py
    removed en check.

--- cp1.py
+++ cp1.py
@@ -7,7 +7,7 @@ import os
 import streamlit as st
 from openai import OpenAI
 import requests
-from langdetect import detect
+# from langdetect import detect
 
 
 
@@ -294,34 +294,31 @@ st.markdown("""
 )
 
 if prompt := bars():
-  if detect(prompt) == "en": 
-      # Store and display the current prompt.
-      st.session_state.messages.append({"role": "user", "content": prompt})
-  
-      with st.chat_message("user"):
-          st.markdown(prompt)
-          
-          
-      # Generate a response using the OpenAI API.
-      stream = client.chat.completions.create(
-          model="gpt-4o",
-          messages=[
-              {"role": m["role"], "content": m["content"]}
-              for m in st.session_state.messages
-          ],
-          stream=True,
-      )
-     
-      # Stream the response to the chat using `st.write_stream`, then store it in 
-      # session state.
-      with st.chat_message("assistant"):
-        response = st.write_stream(stream)
-        prompt.clear()
+  # if detect(prompt) == "en": 
+    # Store and display the current prompt.
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    with st.chat_message("user"):
+        st.markdown(prompt)
         
-          
-      st.session_state.messages.append({"role": "assistant", "content": response})
-  prompt.clear()
-  del prompt
-  
-else:
-  st.write("Please click refresh & give only English prompt either by Typing or through Voice.")
+        
+    # Generate a response using the OpenAI API.
+    stream = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {"role": m["role"], "content": m["content"]}
+            for m in st.session_state.messages
+        ],
+        stream=True,
+    )
+   
+    # Stream the response to the chat using `st.write_stream`, then store it in 
+    # session state.
+    with st.chat_message("assistant"):
+      response = st.write_stream(stream)
+      prompt.clear()
+      
+        
+    st.session_state.messages.append({"role": "assistant", "content": response})
+prompt.clear()
+del prompt
