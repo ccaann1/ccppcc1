40c4055e96b8ace5f07bb8da6436af2306324802
ccaann1 <bhk955@gmail.com>
2025-01-19 20:00:32 -0500

    Update cp1.py

--- cp1.py
+++ cp1.py
@@ -256,29 +256,31 @@ def bars():
     prompt = None
     with col1: prompt1 = st.chat_input(placeholder="Type or Use Mic To Ask Question.")
     with col2:
-        # try:
-        # audio_data.clear()
-        audio_bytes = None
-        audio_bytes = audio_recorder(
-        text="",
-        recording_color="red",
-        neutral_color="green",
-        icon_name="microphone",
-        icon_size="2x",
-        )
-        if audio_bytes:
-            # st.write("Prompt taken thru voice...")
-            audio_file = io.BytesIO(audio_bytes)
-            recognizer = speech.Recognizer()
-            with speech.AudioFile(audio_file) as source:
-                audio_data = recognizer.record(source)  # Read the entire audio file
-                prompt2 = recognizer.recognize_google(audio_data).lower()
-            
-            # audio_data.clear()
-            audio_data = None
+        if st.button("Mic"):
+          # try:
+          # audio_data.clear()
+          audio_bytes = None
+          audio_bytes = audio_recorder(
+          text="",
+          recording_color="red",
+          neutral_color="green",
+          icon_name="microphone",
+          icon_size="2x",
+          )
+          if audio_bytes:
+              # st.write("Prompt taken thru voice...")
+              audio_file = io.BytesIO(audio_bytes)
+              recognizer = speech.Recognizer()
+              with speech.AudioFile(audio_file) as source:
+                  audio_data = recognizer.record(source)  # Read the entire audio file
+                  prompt2 = recognizer.recognize_google(audio_data).lower()
+              
+              # audio_data.clear()
+              audio_data = None
       
         else:
             st.write("Click on Mic Symbol to Talk With Isha.")
+    
     if len(prompt1) > 0:
       prompt = prompt1
     else:
